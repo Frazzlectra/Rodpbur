e5fac332f19843ee3c92a492f7a72071b7f6f9dd
Frazzlectra <baherndon16@tfa.edu>
2015-04-15 09:51:38 -0500

    Initiation
    I have created the basics for ground spawning and movement.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+    GameObject spawnPoint;
+    public GameObject ground;
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this.gameObject.GetComponent<GameManager>();
+        spawnPoint = GameObject.FindGameObjectWithTag("mainSpawnPoint");
+    }
+
+    public void SpawnGround()
+    {
+        Instantiate(ground, spawnPoint.transform.position, Quaternion.identity);
+    }
+
+}
